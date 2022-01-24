900eed8db885aaa390c9f14bbb1e321fd817ed8f
Mikolaj-Figiel <78086404+Mikolaj-Figiel@users.noreply.github.com>
2022-01-24 17:56:24 +0000

    updated main for practical 1
    added code in practical 1 in order to generate a different output

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -2,8 +2,17 @@
 
 int main(){
   sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  sf::RectangleShape box(sf::Vector2f(200.f, 200.f));
+  box.setFillColor(sf::Color::Red);
+  sf::CircleShape shape(10.f);
+  shape.setFillColor(sf::Color::Blue);
+  shape.setPosition(sf::Vector2f(50.f, 40.f));
+  sf::CircleShape shape2(10.f);
+  shape2.setFillColor(sf::Color::Blue);
+  shape2.setPosition(sf::Vector2f(150.f, 40.f));
+  sf::RectangleShape mouth(sf::Vector2f(100.f, 50.f));
+  mouth.setFillColor(sf::Color::Green);
+  mouth.setPosition(sf::Vector2f(40.f, 120.f));
 
   while (window.isOpen()){
       sf::Event event;
@@ -13,7 +22,15 @@ int main(){
       }
     }
     window.clear();
+    window.draw(box);
     window.draw(shape);
+
+    // you cannot draw a shape, move it, then draw it again and expect the first one to stay there.
+    // you need multiple objects with the same properties to do that
+    //shape.setPosition(sf::Vector2f(150.f, 40.f));
+
+    window.draw(shape2);
+    window.draw(mouth);
     window.display();
   }
   return 0;
