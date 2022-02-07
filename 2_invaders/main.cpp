29e6b4351a0918c713df48ecc023124f98b864bd
Mikolaj-Figiel <78086404+Mikolaj-Figiel@users.noreply.github.com>
2022-02-07 17:35:23 +0000

    updated cmakelists file
    added copy_resources function to cmake, added change working dir to cmakelists file

--- 2_invaders/main.cpp
+++ 2_invaders/main.cpp
@@ -10,24 +10,15 @@ void Render() {
 }
 
 
-int main(){
-  sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
- 
-  while (window.isOpen()){
-      sf::Event event;
-      while (window.pollEvent(event)){
-      if (event.type == sf::Event::Closed){
-        window.close();
-      }
-    }
-    window.clear();
-
-    // you cannot draw a shape, move it, then draw it again and expect the first one to stay there.
-    // you need multiple objects with the same properties to do that
-    //shape.setPosition(sf::Vector2f(150.f, 40.f));
-
-
-    window.display();
-  }
-  return 0;
+int main() {
+	//initialise and load
+	bool shouldQuit = false;
+	float dt;
+	while (!shouldQuit) {
+		//Caluclate dt
+		//Update(dt);
+		//Render();
+		//Wait for Vsync
+	}
+	//Unload and shutdown
 }
