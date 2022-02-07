7271fd229a7cc4c46b06a58c909d0ea279158cee
Mikolaj-Figiel <78086404+Mikolaj-Figiel@users.noreply.github.com>
2022-02-07 17:22:10 +0000

    added invader spritesheet
    added invader spritesheet to res folder

--- 2_invaders/main.cpp
+++ 2_invaders/main.cpp
@@ -1,19 +1,18 @@
 #include <SFML/Graphics.hpp>
 
-int main(){
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::RectangleShape box(sf::Vector2f(200.f, 200.f));
-  box.setFillColor(sf::Color::Red);
-  sf::CircleShape shape(10.f);
-  shape.setFillColor(sf::Color::Blue);
-  shape.setPosition(sf::Vector2f(50.f, 40.f));
-  sf::CircleShape shape2(10.f);
-  shape2.setFillColor(sf::Color::Blue);
-  shape2.setPosition(sf::Vector2f(150.f, 40.f));
-  sf::RectangleShape mouth(sf::Vector2f(100.f, 50.f));
-  mouth.setFillColor(sf::Color::Green);
-  mouth.setPosition(sf::Vector2f(40.f, 120.f));
 
+void Update(double dt) {
+    // Update Everything
+}
+
+void Render() {
+    // Draw Everything
+}
+
+
+int main(){
+  sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+ 
   while (window.isOpen()){
       sf::Event event;
       while (window.pollEvent(event)){
@@ -22,15 +21,12 @@ int main(){
       }
     }
     window.clear();
-    window.draw(box);
-    window.draw(shape);
 
     // you cannot draw a shape, move it, then draw it again and expect the first one to stay there.
     // you need multiple objects with the same properties to do that
     //shape.setPosition(sf::Vector2f(150.f, 40.f));
 
-    window.draw(shape2);
-    window.draw(mouth);
+
     window.display();
   }
   return 0;
