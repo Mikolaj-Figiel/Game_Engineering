83f3a8029fffcbb9b565320ebdb233a3eec16b28
Mikolaj-Figiel <78086404+Mikolaj-Figiel@users.noreply.github.com>
2022-02-14 11:42:52 +0000

    updated main.cpp for invaders practical

--- 2_invaders/main.cpp
+++ 2_invaders/main.cpp
@@ -1,5 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
+sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+sf::Texture spritesheet;
+sf::Sprite invader;
 
 void Update(double dt) {
     // Update Everything
@@ -7,17 +11,27 @@ void Update(double dt) {
 
 void Render() {
     // Draw Everything
+	window.draw(invader);
 }
 
 
 int main() {
 	//initialise and load
+
 	bool shouldQuit = false;
 	float dt;
+
+	
+		if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
+			std::cerr << "Failed to load spritesheet!" << std::endl;
+		}
+		invader.setTexture(spritesheet);
+		invader.setTextureRect(sf::IntRect(sf::Vector2(0, 0), sf::Vector2(32, 32)));
+	
 	while (!shouldQuit) {
 		//Caluclate dt
 		//Update(dt);
-		//Render();
+		//Render(window);
 		//Wait for Vsync
 	}
 	//Unload and shutdown
